c68590674cdeb2f7c5dfff06257932b668394263
Ishwarya Lakshmi <ishwaryalakshmi@APPLEs-MacBook-Pro.local>
2025-01-26 00:26:54 +0530

    server port update

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const server = http.createServer((request, response) => {
     }
 })
 
-server.listen(3000, () => {
+server.listen(3000,  '0.0.0.0', () => {
     console.log('Server running on port 3000')
 })
